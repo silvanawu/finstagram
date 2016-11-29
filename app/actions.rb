b50c281114e941ea42519586553872352cac859e
Silvana Wu <silvana_wu@hotmail.com>
2016-11-29 03:07:41 +0000

    app

--- app/actions.rb
+++ app/actions.rb
@@ -1,3 +1,9 @@
+helpers do
+    def current_user
+        User.find_by(id: session[:user_id])
+    end
+end
+
 def humanized_time_ago(time_ago_in_minutes)
     if time_ago_in_minutes >= 60
         "#{time_ago_in_minutes / 60} hours ago"
@@ -67,9 +73,39 @@ post '/signup' do
     @user = User.new({ email: email, avatar_url: avatar_url, username: username, password: password})
     
     if @user.save
-      "User #{username} saved!"
+      redirect to('/login')
     else
-    erb(:signup)
+      erb(:signup)
     end
 end
 
+get '/login' do
+  erb(:login)
+  #params.to_s
+end
+
+post '/login' do
+    username = params[:username]
+    password = params[:password]
+    
+    user = User.find_by(username: username)
+    
+    if user && user.password == password
+        session[:user_id] = user.id
+        redirect to('/')
+    else
+        @error_message = "Login Failed"
+        erb(:login)
+    end
+end
+
+get '/logout' do
+    session[:user_id] = nil
+    redirect to('/')
+end
+
+get '/' do
+    @posts = Post.order(created_at: :desc)
+    erb(:index)
+end
+    
